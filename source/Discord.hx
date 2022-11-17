94a4625d2d4d33a0427ad090f447c8887ad98e1b
flavor <68028420+boris2010z@users.noreply.github.com>
2022-11-16 23:46:52 -0800

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -17,7 +17,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1042706386552885278",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
